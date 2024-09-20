2f8565a4433a0190c6d15810a55bea7885adfcc0
Bryan Van Hal <FrogmanL@live.com>
2024-09-19 21:45:38 -0400

    Added methods to determine how to split a document and added option for 'stuffing' method of summarization.

--- llm_summarize.py
+++ llm_summarize.py
@@ -20,35 +20,36 @@ def load_pdf(file_path):
     documents = loader.load()
     
     # Split documents into manageable chunks for processing
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
-    docs = text_splitter.split_documents(documents)
-    
+    #docs = split_doc_by_char(documents, 8000, 100)
+    docs = split_doc_by_page(documents, 21)
+
     end = time.time()
     print(f"Loaded PDF in {(end-start):.2f} seconds")
 
     return docs
 
-# Summarization function that chains map, reduce, and stuff methods
-from langchain_core.output_parsers import StrOutputParser
-def chained_summarization(docs):
-    
-    llm = ChatOpenAI(
+def split_doc_by_char(documents, chunkSize, chunkOverlap):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunkSize, chunk_overlap=chunkOverlap)
+    return text_splitter.split_documents(documents)
+
+def split_doc_by_page(documents, endPage):
+    lastPage = len(documents) - endPage
+    return documents[:-lastPage]
+
+def define_llm():
+    return ChatOpenAI(
         temperature=0.1,
         model_name="tinyllama:latest",
         api_key="ollama",
         #base_url="http://192.168.1.137:11434/v1",
         base_url="http://localhost:11434/v1",
     )
+
+# Summarization function that chains map and reduce methods
+def map_reduce_summarization(docs):
     
-    prompt_template = """Write a long summary of the following document. 
-    Only include information that is part of the document. 
-    Do not include your own opinion or analysis.
-
-    Use this content:
-    "{context}"
-    Summary:"""
-    prompt = PromptTemplate.from_template(prompt_template)
-
+    llm = define_llm()
+    
     # Step 1: Use the 'map' approach to create a summary for each chunk
     print(f"Mapping ...")
     start = time.time()
@@ -68,15 +69,32 @@ def chained_summarization(docs):
 
     return final_summary
 
-# Main function to run the chained summarization
+# Summarization function that chains stuff method
+def stuff_summarization(docs):
+    
+    llm = define_llm()
+    
+    print(f"Stuffing ...")
+    start = time.time()
+    map_chain = load_summarize_chain(llm, chain_type="stuff")
+    final_summary = map_chain.invoke(docs)
+    end = time.time()
+    print(f"Stuffed in {(end-start):.2f} seconds")
+
+    return final_summary
+
+# Main function to run the summarization
 def main(pdf_file):
     print(f"Summarizing ...")
     start = time.time()
     # Load the PDF file
     docs = load_pdf(pdf_file)
     
-    # Apply the chained summarization
-    final_summary = chained_summarization(docs[:-3])
+    # Apply the stuff summarization
+    final_summary = stuff_summarization(docs)
+
+    # Apply the map/reduce summarization
+    #final_summary = map_reduce_summarization(docs)
 
     end = time.time()
     print(f"Summarized in {(end-start):.2f} seconds")
